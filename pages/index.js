3e14374dc2eb89aa3101e8f6dee6a48b909c8a1a
ravikaushik151 <ravi.k@imsolutions.mobi>
2024-07-17 12:05:42 +0530

    commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// pages/index.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+    </div>
+  );
+};
+
+export default HomePage;
